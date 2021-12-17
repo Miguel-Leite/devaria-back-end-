082d41df37639f55245f94e6583475dcabe0270d
Miguel-Leite <miguelleite200leite@gmail.com>
2021-12-17 20:33:12 +0100

    add news routers, docs, auth

--- Core/urls.py
+++ Core/urls.py
@@ -18,7 +18,21 @@ from django.urls import path, include
 
 from django.views.decorators.csrf import csrf_exempt
 
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('Api.urls'))
+    path('api/', include('Api.urls')),
+    
+    path('api/docs/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
 ]
