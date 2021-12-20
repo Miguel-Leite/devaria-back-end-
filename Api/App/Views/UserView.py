77003c3ba060c00aa3a20615c0841e92f6760dfd
Miguel-Leite <miguelleite200leite@gmail.com>
2021-12-20 20:43:37 +0100

    comment code login

--- Api/App/Views/UserView.py
+++ Api/App/Views/UserView.py
@@ -24,12 +24,12 @@ class RegisterAPI(generics.GenericAPIView):
 	        "token": AuthToken.objects.create(user)[1]
         })
 
-class LoginAPI(KnoxLoginView):
-    permission_classes = (permissions.AllowAny,)
+# class LoginAPI(KnoxLoginView):
+#     permission_classes = (permissions.AllowAny,)
 
-    def post(self, request, format=None):
-        serializer = AuthTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        login(request, user)
-        return super(LoginAPI, self).post(request, format=None)
+#     def post(self, request, format=None):
+#         serializer = AuthTokenSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data['user']
+#         login(request, user)
+#         return super(LoginAPI, self).post(request, format=None)
