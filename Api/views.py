6313c5d867c9d690e2d344564143a0a260438f62
Miguel-Leite <miguelleite200leite@gmail.com>
2021-12-21 08:13:11 +0100

    views for users

--- Api/views.py
+++ Api/views.py
@@ -1,13 +1,45 @@
+from django.db import connection
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-
 from Api.App.Serializer.UserSerializer import RegisterSerializer
+from Api.App.Serializer.UserSerializer import UserSerializer
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+
+from Api.models import User
+
+
+@api_view(['GET'])
+def getusers_view(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users,many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def userDetail(request, id):
+	user = User.objects.filter(id=id).values()
+	serializer = UserSerializer(user, many=True)
+	return Response(serializer.data)
 
 @api_view(['POST'])
-def registration_view(request):
-    
+def userUpdate(request, id):
+	try:
+		user = User.objects.get(id=id)
+		serializer = UserSerializer(instance=user,data=request.data)
+		
+		if serializer.is_valid():
+			serializer.save()
+
+		return Response(serializer.data)
+
+	except Exception as e:
+		return "Falha ao actualizar o modulo!"
+
+@api_view(['POST'])
+def registration_view(request):    
     if request.method == 'POST':
         serializer = RegisterSerializer(data=request.data)
         data ={}
@@ -16,8 +48,29 @@ def registration_view(request):
             data['response'] = True
             data['email'] = account.email
             data['username'] = account.username
+            return Response({'response': True})
         else:
             data['response'] = False
             data = serializer.errors
-
         return Response(data)
+    
+@api_view(['DELETE'])
+def delete_view(request,id):
+    try:
+        user = User.objects.get(pk=id)
+        user.delete()
+        return Response({'response': True})
+    except User.DoesNotExist:
+        return Response({'response': None})
+    
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteAll_view(request,yes):
+    try:
+        if yes == 'yes':
+            user = User.objects.all()
+            user.delete()
+        return Response({'response': True})
+    except User.DoesNotExist:
+        return Response({'response': None})
+    
