3734f476c25dcaa5f31138698a892d8d588d593e
Miguel-Leite <miguelleite200leite@gmail.com>
2021-12-21 08:12:26 +0100

    alter tables

--- Api/models.py
+++ Api/models.py
@@ -13,13 +13,13 @@ class User(AbstractUser):
 
 class Module(models.Model):
     id = models.CharField(max_length=150, primary_key=True, default=uuid4, editable=False)
-    name = models.CharField(max_length=100, null=True)
+    module = models.CharField(max_length=100, null=True)
     created_at =  models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
 
 class Courses(models.Model):
     id=models.CharField(max_length=150, primary_key=True, default=uuid4, editable=False)
-    name=models.CharField(max_length=100)
+    course=models.CharField(max_length=100)
     module= models.ForeignKey('Module', on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
